883d5a598a1e053eb9ca3f02123f1815405e7d09
sanket <sanketsabale9767@gmail.com>
2023-02-17 19:07:12 +0530

    Connected our app to db

---
+++ understanding_jpa/src/test/java/com/sanketscode/learning_jpa/understanding_jpa/UnderstandingJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sanketscode.learning_jpa.understanding_jpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UnderstandingJpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
